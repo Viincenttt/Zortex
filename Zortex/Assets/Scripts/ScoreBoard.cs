f8581685e9c7177e3eacd089e17f0c0df3e8d477
Vincent <vincentkok@live.nl>
2018-05-27 20:51:24 +0200

    Started working on the score system that will track the players score

---
+++ Zortex/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+    public class ScoreBoard : MonoBehaviour {
+        public int _currentScore = 0;
+
+        public int Score {
+            get { return this._currentScore; }
+        }
+        
+        public void ResetScore() {
+            this._currentScore = 0;
+        }
+
+        public void OnEnemyKilled() {
+            this._currentScore++;
+        }
+    }
+}
