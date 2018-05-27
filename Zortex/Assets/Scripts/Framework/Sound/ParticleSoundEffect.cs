ee380d82210dc6000a36886cc651a95f7f1408e1
Vincent <vincentkok@live.nl>
2018-05-27 14:14:21 +0200

    Added a sound effect to the players particle system laser

---
+++ Zortex/Assets/Scripts/Framework/Sound/ParticleSoundEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Framework.Sound {
+    [RequireComponent(typeof(ParticleSystem))]
+    [RequireComponent(typeof(AudioSource))]
+    public class ParticleSoundEffect : MonoBehaviour {
+        [SerializeField] private AudioClip _onEmitSoundEffect;
+
+        private ParticleSystem _particleSystem;
+        private AudioSource _audioSource;
+
+        private int _lastNumberOfParticles = 0;
+
+        void Start () {
+            this._particleSystem = this.GetComponent<ParticleSystem>();
+            this._audioSource = this.GetComponent<AudioSource>();
+        }
+
+        void Update () {
+            if (this.HasParticleSpawnedSinceLastUpdate) {
+                this.PlayOnEmitSoundEffect();
+            }
+
+            this._lastNumberOfParticles = this._particleSystem.particleCount;
+        }
+
+        private void PlayOnEmitSoundEffect() {
+            this._audioSource.PlayOneShot(this._onEmitSoundEffect);
+        }
+
+        private bool HasParticleSpawnedSinceLastUpdate {
+            get { return this._particleSystem.particleCount > this._lastNumberOfParticles; }
+        }
+    }
+}
