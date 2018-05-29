18b1ec46d021682ac9b9d115e8fcf110c330118e
Vincent <vincentkok@live.nl>
2018-05-29 20:33:26 +0200

    Fixed bug that caused the player ship to fire a single shot when starting the game

--- Zortex/Assets/Scripts/Actors/Player/PlayerGun.cs
+++ Zortex/Assets/Scripts/Actors/Player/PlayerGun.cs
@@ -30,11 +30,14 @@ namespace Assets.Scripts.Actors.Player {
         }
 
         private void StartFiring() {
+            // Because play on awake is set to false, we need to call play the first time to get it working. 
+            this._gun.Play();
+
             this.SetGunEmissionModuleEnabled(true);
         }
 
         private void SetGunEmissionModuleEnabled(bool enabled) {
-            // We can't use ParticleSystem.Stop/Start for this, because this will wait untill all particles have died before
+            // We can't use ParticleSystem.Stop/Play for this, because this will wait untill all particles have died before
             // starting the particlesystem. A quick fix for this to enable/disable the emissionmodule of the particle system.
             ParticleSystem.EmissionModule emissionModule = this._gun.emission;
             emissionModule.enabled = enabled;
